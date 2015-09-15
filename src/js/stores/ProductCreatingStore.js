3e96da6fbf0d08efe0d6bf71a6720c2603c4e4c1
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-09-15 12:11:17 +0800

    Add route for products/new

---
+++ src/js/stores/ProductCreatingStore.js
@@ -0,0 +1,20 @@
+import alt from '../alt';
+import ProductCreatingActions from '../actions/ProductCreatingActions';
+
+class ProductCreatingStore {
+  constructor() {
+    this.image = null;
+    this.errors = null;
+    this.bindListeners({
+      handleUploadImage: ProductCreatingActions.UPLOAD_IMAGE
+    });
+  }
+
+  handleUploadImage(image) {
+    this.image = image;
+  }
+
+}
+
+export default alt.createStore(ProductCreatingStore, 'ProductCreatingStore');
+
